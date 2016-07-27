afb9bf7b387e98a65f252d5cb59023ed1a7d28df
boyley <136397156@qq.com>
2016-07-27 15:25:47 +0800

    spring-cloud-simple

---
+++ spring-cloud-simple-helloworld/src/main/java/com/example/controller/SampleController.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by huilv on 2016/7/27.
+ */
+@Controller
+@SpringBootApplication
+public class SampleController {
+    @ResponseBody
+    @RequestMapping(value = "/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
